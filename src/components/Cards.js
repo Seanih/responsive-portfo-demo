9d92d3e595cd6753562fd326c03b8dad13dd61ee
Sean Anih <sean.anih@gmail.com>
2020-12-27 18:43:12 -0500

    added outstanding components/styles and completed the deemo project

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import CardItem from './CardItem';
 import '../styles/Cards.scss';
+import waterfall_pic from '../images/img-9.jpg';
+import island_pic from '../images/img-2.jpg';
+import boats_pic from '../images/img-3.jpg';
+import field_pic from '../images/img-4.jpg';
+import fancy_pic from '../images/img-5.jpg';
 
 const Cards = () => {
 	return (
@@ -9,7 +14,38 @@ const Cards = () => {
 			<div className='cards__container'>
 				<div className='cards__wrapper'>
 					<ul className='cards__items'>
-						<CardItem />
+						<CardItem
+							src={waterfall_pic}
+							text="waterfalls you'd wanna skinny dip in!"
+							label='Adventure'
+							path='/services'
+						/>
+						<CardItem
+							src={island_pic}
+							text='beautiful island!'
+							label='Luxury'
+							path='/services'
+						/>
+					</ul>
+					<ul className='cards__items'>
+						<CardItem
+							src={boats_pic}
+							text='Speed Boats'
+							label='Activities'
+							path='/services'
+						/>
+						<CardItem
+							src={field_pic}
+							text='exclusive sports areas'
+							label='Sports'
+							path='/services'
+						/>
+						<CardItem
+							src={fancy_pic}
+							text='5 star restaurants'
+							label='Luxury'
+							path='/services'
+						/>
 					</ul>
 				</div>
 			</div>
---
+++ src/components/pages/Products.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../../styles/App.css';
+
+const Products = () => {
+	return <h1 className='products'>Products</h1>;
+};
+
+export default Products;
